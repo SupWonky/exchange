952b756f0edf96bbbb038b704002111276f11e3d
development <dev@mail.com>
2025-06-23 17:00:57 +0100

    add icon

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,10 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap",
   },
+  {
+    rel: "icon",
+    href: "/favicon.ico",
+  },
 ];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
